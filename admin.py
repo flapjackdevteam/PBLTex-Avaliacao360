6d0259bd62ccc0d43ad8f44633b98cdd6d24910f
Julio Cesar Barbosa da Rosa Rodrigues <juliojcbarbosa2003@gmail.com>
2023-05-23 13:16:08 -0300

    Funcionalidade da tela do adm com as funcoes de adicionar novo user,editar e remover. E atualiazação da base de dados json com a implementação de turmas e times.

--- admin.py
+++ admin.py
@@ -1,63 +1,198 @@
 import PySimpleGUI as sg
-import csv
 import json
 
 # Carrega um tema pré definido do PySimpleGui
 sg.theme('DefaultNoMoreNagging')
 
-def adicionar_usuario():
-    # Lógica para adicionar um usuário
-    print("Adicionar usuário")
-
-def remover_usuario():
-    # Lógica para remover um usuário
-    print("Remover usuário")
-
 def tela_administracao():
-    print("Abrindo a tela de administração!")
-
-    # Carrega o conteúdo do arquivo data.json em um dicionário Python
-    with open('data.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
-
-def tela_admin():
-    admin_layout = [
-                [sg.Text('Login do Administrador', font=('Helvetica', 30), justification='center')],
-                [sg.Text('Senha:', font=('Helvetica', 20)), sg.InputText(password_char='*', font=('Helvetica', 20))],
-                [sg.Button('Confirmar', font=('Helvetica', 20), button_color=('white', 'black'), size=(10, 1))]
+    # Layout da tela de administração
+    layout = [
+        [sg.Text("Gerenciamento do Administrador", font=("Helvetica", 16))],
+        [sg.Button("Adicionar usuário", size=(20, 2), key="-ADICIONAR-")],
+        [sg.Button("Selecionar usuário", size=(20, 2), key="-SELECIONAR-")],
+        [sg.Button("Sair", size=(10, 1))]
     ]
 
-    gerenciamento_layout = [
-            [sg.Text('Página de Gerenciamento', font=('Helvetica', 30), justification='center')],   
-            [sg.Text("Gerenciamento do Administrador", font=("Helvetica", 16))],
-            [sg.Button("Adicionar usuário", size=(20, 2),border_width=3)],
-            [sg.Button("Remover usuário", size=(20, 2),border_width=3)],
-            [sg.Button("Sair", size=(20, 2),border_width=3)]
-            # Aqui você pode adicionar outros elementos de layout para a janela de gerenciamento
-    ]
+    # Criação da janela da tela de administração
+    window = sg.Window("Painel do Administrador", layout)
 
-    # Cria a janela de gerenciamento com o tamanho padrão da tela
-    gerenciamento_janela = sg.Window('Tela do Administrador', gerenciamento_layout,
-                                        finalize=True, resizable=True, element_padding=(50, 50),
-                                        keep_on_top=True)
-    
-    # Definir o tamanho mínimo da janela
-    gerenciamento_janela.set_min_size((800, 600))
-
-    # Loop para interagir com os eventos da janela
+    # Loop de eventos da janela
     while True:
-        event, values = gerenciamento_janela.read()
+        event, values = window.read()
 
-        # Se o usuário fechar a janela ou clicar em "Sair"
         if event == sg.WINDOW_CLOSED or event == "Sair":
             break
-        # Se o usuário clicar em "Adicionar usuário"
-        elif event == "Adicionar usuário":
+        elif event == "-ADICIONAR-":
             adicionar_usuario()
-        # Se o usuário clicar em "Remover usuário"
-        elif event == "Remover usuário":
-            remover_usuario()
+        elif event == "-SELECIONAR-":
+            selecionar_usuario()
 
-    # Fechar a janela quando sair do loop
-    gerenciamento_janela.close()
+    window.close()
 
+# Solicitar informações do novo usuário
+def adicionar_usuario():
+    layout_admin = [
+        [sg.Text("Nome:      "), sg.Input(key="-NOME-", border_width=2, size=(25, 1))],
+        [sg.Text("Matrícula: "), sg.Input(key="-MATRICULA-", border_width=2, size=(17, 1))],
+        [sg.Text("Turma:     "), sg.Input(key="-TURMA-", border_width=2, size=(10, 1))],
+        [sg.Text("Times:     "), sg.Input(key="-TIMES-", border_width=2, size=(10, 1))],
+        [sg.Button("Adicionar", size=(10, 1))]
+    ]
+
+    window_admin = sg.Window("Adicionar Usuário", layout_admin)
+
+    while True:
+        event_admin, values_admin = window_admin.read()
+
+        if event_admin == sg.WINDOW_CLOSED or event_admin == "Adicionar":
+            break
+
+    window_admin.close()
+
+    # Carregar usuários existentes do arquivo JSON
+    with open("data.json", "r") as arquivo:
+        data = json.load(arquivo)
+
+    usuarios = data["usuarios"]
+
+    # Criar usuário com as informações digitadas
+    novo_usuario = {
+        "nome": values_admin["-NOME-"],
+        "matricula": values_admin["-MATRICULA-"],
+        "turma": values_admin["-TURMA-"],
+        "times": values_admin["-TIMES-"]
+    }
+
+    # Adicionar o novo usuário à lista existente
+    usuarios.append(novo_usuario)
+
+    # Salvar a lista de usuários atualizada no arquivo JSON
+    with open("data.json", "w") as arquivo:
+        json.dump(data, arquivo)
+
+    sg.popup("Usuário adicionado com sucesso!")
+
+def editar_usuario(nome_usuario):
+    # Carregar usuários do arquivo JSON
+    with open("data.json", "r") as arquivo:
+        data = json.load(arquivo)
+
+    usuarios = data["usuarios"]
+
+    # Procurar usuário pelo nome
+    for usuario in usuarios:
+        if usuario["nome"] == nome_usuario:
+            # Solicitar novas informações do usuário
+            layout_admin = [
+                [sg.Text("Novo nome: "), sg.Input(key="-NOVO_NOME-", default_text=usuario["nome"])],
+                [sg.Text("Nova matrícula: "), sg.Input(key="-NOVA_MATRICULA-", default_text=usuario["matricula"])],
+                [sg.Button("Atualizar")]
+            ]
+
+            window_admin = sg.Window("Editar Usuário", layout_admin)
+
+            while True:
+                event_admin, values_admin = window_admin.read()
+
+                if event_admin == sg.WINDOW_CLOSED or event_admin == "Atualizar":
+                    break
+
+            window_admin.close()
+
+            # Atualizar informações do usuário
+            usuario["nome"] = values_admin["-NOVO_NOME-"]
+            usuario["matricula"] = values_admin["-NOVA_MATRICULA-"]
+
+            # Salvar usuários atualizados no arquivo JSON
+            with open("data.json", "w") as arquivo:
+                json.dump(data, arquivo)
+
+            sg.popup("Usuário atualizado com sucesso!")
+            return
+
+    sg.popup("Usuário não encontrado!")
+
+def remover_usuario(nome_usuario):
+    # Carregar usuários do arquivo JSON
+    with open("data.json", "r") as arquivo:
+        data = json.load(arquivo)
+
+    usuarios = data["usuarios"]
+
+    # Procurar usuário pelo nome
+    for usuario in usuarios:
+        if usuario["nome"] == nome_usuario:
+            usuarios.remove(usuario)
+
+            # Salvar usuários atualizados no arquivo JSON
+            with open("data.json", "w") as arquivo:
+                json.dump(data, arquivo)
+
+            sg.popup("Usuário removido com sucesso!")
+            return
+
+    sg.popup("Usuário não encontrado!")
+
+def selecionar_usuario():
+    # Carregar usuários do arquivo JSON
+    with open("data.json", "r") as arquivo:
+        data = json.load(arquivo)
+
+    usuarios = data["usuarios"]
+
+    # Criar layout da lista de usuários
+    layout_lista = [
+        [sg.Listbox(values=[usuario["nome"] for usuario in usuarios], size=(30, 6), key="-LISTA-")],
+        [sg.Button("Editar", key="-EDITAR-"), sg.Button("Remover", key="-REMOVER-")]
+    ]
+
+    window_lista = sg.Window("Selecione um Usuário", layout_lista)
+
+    while True:
+        event_lista, values_lista = window_lista.read()
+
+        if event_lista == sg.WINDOW_CLOSED:
+            break
+        elif event_lista == "-EDITAR-":
+            # Verificar se um usuário foi selecionado
+            if len(values_lista["-LISTA-"]) > 0:
+                usuario_selecionado = values_lista["-LISTA-"][0]
+                editar_usuario(usuario_selecionado)
+                break
+            else:
+                sg.popup("Nenhum usuário selecionado!")
+        elif event_lista == "-REMOVER-":
+            # Verificar se um usuário foi selecionado
+            if len(values_lista["-LISTA-"]) > 0:
+                usuario_selecionado = values_lista["-LISTA-"][0]
+                remover_usuario(usuario_selecionado)
+                break
+            else:
+                sg.popup("Nenhum usuário selecionado!")
+
+    window_lista.close()
+
+
+# Layout da interface
+layout = [
+    [sg.Text("Gerenciamento do Administrador", font=("Helvetica", 16))],
+    [sg.Button("Adicionar usuário", size=(20, 2), key="-ADICIONAR-")],
+    [sg.Button("Selecionar usuário", size=(20, 2), key="-SELECIONAR-")],
+    [sg.Button("Sair", size=(10, 1))]
+]
+
+# Criação da janela
+window = sg.Window("Painel do Administrador", layout)
+
+# Loop de eventos da janela
+while True:
+    event, values = window.read()
+
+    if event == sg.WINDOW_CLOSED or event == "Sair":
+        break
+    elif event == "-ADICIONAR-":
+        adicionar_usuario()
+    elif event == "-SELECIONAR-":
+        selecionar_usuario()
+
+window.close()
